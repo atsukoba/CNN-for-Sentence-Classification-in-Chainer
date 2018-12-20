7df042abecd4db99c8422b6b60bb9336647240b8
atsukoba <atsuya_wgp4346@keio.jp>
2018-12-20 15:36:00 +0900

    edit and add embed model file

--- neuralnets.py
+++ neuralnets.py
@@ -10,8 +10,98 @@ import chainer.functions as F
 import chainer.links as L
 from chainer.training import extensions
 
+class CNN_rand(Chain):
+    """
+    Chain of CNN for Sentence classification model.
+    """
+    def __init__(self):
+        super(CNNSC, self).__init__()
+        with self.init_scope():
+            # Convolutions
+        self.embed = L.(
+                in_channels=1, out_channels=6, ksize=5, stride=1)
+            self.conv2 = L.Convolution2D(
+                in_channels=6, out_channels=16, ksize=5, stride=1)
+            self.conv3 = L.Convolution2D(
+                in_channels=16, out_channels=120, ksize=4, stride=1)
+            # full connection
+            self.fc4 = L.Linear(None, 84)
+            self.fc5 = L.Linear(84, 10)
 
-class CNNSC(Chain):
+    def forward(self, x):
+        h = F.sigmoid(self.conv1(x))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv2(h))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv3(h))
+        h = F.sigmoid(self.fc4(h))
+        if chainer.config.train:
+            return self.fc5(h)
+        return F.softmax(self.fc5(h))
+
+
+class CNN_static(Chain):
+    """
+    Chain of CNN for Sentence classification model.
+    """
+    def __init__(self):
+        super(CNNSC, self).__init__()
+        with self.init_scope():
+            # Convolutions
+        self.embed = L.(
+                in_channels=1, out_channels=6, ksize=5, stride=1)
+            self.conv2 = L.Convolution2D(
+                in_channels=6, out_channels=16, ksize=5, stride=1)
+            self.conv3 = L.Convolution2D(
+                in_channels=16, out_channels=120, ksize=4, stride=1)
+            # full connection
+            self.fc4 = L.Linear(None, 84)
+            self.fc5 = L.Linear(84, 10)
+
+    def forward(self, x):
+        h = F.sigmoid(self.conv1(x))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv2(h))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv3(h))
+        h = F.sigmoid(self.fc4(h))
+        if chainer.config.train:
+            return self.fc5(h)
+        return F.softmax(self.fc5(h))
+
+class CNN_non_static(Chain):
+    """
+    Chain of CNN for Sentence classification model.
+    """
+    def __init__(self):
+        super(CNNSC, self).__init__()
+        with self.init_scope():
+            # Convolutions
+        self.embed = L.(
+                in_channels=1, out_channels=6, ksize=5, stride=1)
+            self.conv2 = L.Convolution2D(
+                in_channels=6, out_channels=16, ksize=5, stride=1)
+            self.conv3 = L.Convolution2D(
+                in_channels=16, out_channels=120, ksize=4, stride=1)
+            # full connection
+            self.fc4 = L.Linear(None, 84)
+            self.fc5 = L.Linear(84, 10)
+
+    def forward(self, x):
+        h = F.sigmoid(self.conv1(x))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv2(h))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv3(h))
+        h = F.sigmoid(self.fc4(h))
+        if chainer.config.train:
+            return self.fc5(h)
+        return F.softmax(self.fc5(h))
+
+class CNN_multi_ch(Chain):
+    """
+    Chain of CNN for Sentence classification model.
+    """
     def __init__(self):
         super(CNNSC, self).__init__()
         with self.init_scope():
