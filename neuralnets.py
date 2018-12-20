d06686a80f034be2db7ac0018ab033d81038c587
atsukoba <atsuya_wgp4346@keio.jp>
2018-12-20 14:56:34 +0900

    add scripts file

---
+++ neuralnets.py
@@ -0,0 +1,38 @@
+import numpy as np
+import chainer
+from chainer import backend
+from chainer import backends
+from chainer.backends import cuda
+from chainer import Function, gradient_check, report, training, utils, Variable
+from chainer import datasets, iterators, optimizers, serializers
+from chainer import Link, Chain, ChainList
+import chainer.functions as F
+import chainer.links as L
+from chainer.training import extensions
+
+
+class CNN_for_SC(Chain):
+    def __init__(self):
+        super(LeNet5, self).__init__()
+        with self.init_scope():
+            # Convolutions
+            self.conv1 = L.Convolution2D(
+                in_channels=1, out_channels=6, ksize=5, stride=1)
+            self.conv2 = L.Convolution2D(
+                in_channels=6, out_channels=16, ksize=5, stride=1)
+            self.conv3 = L.Convolution2D(
+                in_channels=16, out_channels=120, ksize=4, stride=1)
+            # full connection
+            self.fc4 = L.Linear(None, 84)
+            self.fc5 = L.Linear(84, 10)
+
+    def forward(self, x):
+        h = F.sigmoid(self.conv1(x))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv2(h))
+        h = F.max_pooling_2d(h, 2, 2)
+        h = F.sigmoid(self.conv3(h))
+        h = F.sigmoid(self.fc4(h))
+        if chainer.config.train:
+            return self.fc5(h)
+        return F.softmax(self.fc5(h))
